f9733aaac08bbde9195934e2c5eee1ad0cce94d5
brightonmboya <b.mboya@alustudent.com>
2024-04-16 08:41:22 +0300

    removed unwanted card on the accounting page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,11 @@ import {
   CardTitle,
 } from "~/components/ui/Card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/Tabs";
+import { CalendarDateRangePicker } from "~/components/accounting/date-range-picker";
 import { Overview } from "~/components/accounting/overview";
 import { RecentSales } from "~/components/accounting/recent-sales";
 import ExpenseAndSales from "~/components/accounting/ExpenseAndSales";
+import Reports from "~/components/accounting/reports";
 import Layout from "~/components/Layout/Layout";
 import { api } from "~/utils/api";
 import { Banknote } from "lucide-react";
@@ -115,31 +117,6 @@ export default function DashboardPage() {
                     )}
                   </CardContent>
                 </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">
-                      Active Now
-                    </CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+573</div>
-                    <p className="text-xs text-muted-foreground">
-                      +201 since last hour
-                    </p>
-                  </CardContent>
-                </Card>
               </div>
               <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
                 <Card className="col-span-4">
@@ -153,9 +130,9 @@ export default function DashboardPage() {
                 <Card className="col-span-3">
                   <CardHeader>
                     <CardTitle>Recent Sales</CardTitle>
-                    <CardDescription>
+                    {/* <CardDescription>
                       You made 265 sales this month.
-                    </CardDescription>
+                    </CardDescription> */}
                   </CardHeader>
                   <CardContent>
                     <RecentSales />
